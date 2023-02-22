efff978f1a1c299ca5530b16bd78844d5c8d259c
alex2022cci <alex2022cci@outlook.fr>
2023-02-22 11:46:12 +0100

    placeholder

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,7 +6,9 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
@@ -17,7 +19,24 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('$FirstName', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'First Name'
+                ]
+            ]) 
+            ->add('$LastName' , TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Last Name'
+                ]
+            ]) 
+            ->add('email', Email::class, [
+                'attr' => [
+                    'placeholder' => 'Email Address'
+                ]
+            ])
+            ->add('$Mobile')
+            ->add('$Profile')
+
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
